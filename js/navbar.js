f02d6d4c6b1534db4869294d03b2ed74b148be29
CameronF305 <cameronfields305@gmail.com>
2022-02-11 12:39:33 -0500

    fixed navbar glitches

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,11 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyNav()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var navbar = document.querySelector(".navbar");
+var sticky = navbar.pageYOffset;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyNav() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
