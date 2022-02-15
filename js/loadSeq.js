7e620bf2ab727ee7a2b0fddb53586e492d265231
CameronF305 <cameronfields305@gmail.com>
2022-02-14 21:11:22 -0500

    added a fade animation for the projects page

---
+++ js/loadSeq.js
@@ -0,0 +1,8 @@
+// for projects page
+
+//for use in other pages paste the line below in html
+//<script src="https://unpkg.com/fadin"></script>
+
+document.addEventListener("DOMContentLoaded", () => {
+    fadin('.fade', { delay: 200 })
+  });
