dd5a2ef1c6fe089c2895e4bb19da3c8ebecca17d
CameronF305 <cameronfields305@gmail.com>
2022-02-22 13:38:27 -0500

    waiting until post-launch for fade in effect

--- js/stickNavbar.js
+++ js/stickNavbar.js
@@ -1,6 +1,7 @@
 window.onscroll = function() {stickNavBar()};
           
 var navbar = document.getElementById("navbar");
+var navbar = document.getElementById("navlogo");
 var sticky = navbar.offsetTop;
 
 function stickNavBar() {
