eb0c16456df2235ccc57142d5988e8ce1e9fd154
CameronF305 <cameronfields305@gmail.com>
2022-02-22 16:52:12 -0500

    added tags, prepared for launch

--- js/loadSeq.js
+++
@@ -1,8 +0,0 @@
-// for projects page
-
-//for use in other pages paste the line below in html
-//<script src="https://unpkg.com/fadin"></script>
-
-document.addEventListener("DOMContentLoaded", () => {
-    fadin('.albumElement', { delay: 100 })
-  });
